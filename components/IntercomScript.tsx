8f3fd2b511748167928fcb66be8d0724178889de
Sudhanshu Tiwari <sudhanshutiwari@ourguide.ai>
2026-04-30 16:01:08 +0530

    Fix Intercom script

--- components/IntercomScript.tsx
+++ components/IntercomScript.tsx
@@ -128,9 +128,11 @@ export default function IntercomScript() {
       <Script id="intercom-settings" strategy="afterInteractive">
         {`window.intercomSettings = { api_base: "https://api-iam.intercom.io", app_id: "${APP_ID}" };`}
       </Script>
-      <Script id="intercom-widget" strategy="afterInteractive">
-        {`(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/${APP_ID}';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();`}
-      </Script>
+      <script>
+  {`// We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/hhec07jb'
+  (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/hhec07jb';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();`}
+</script>
     </>
   );
 }
+
